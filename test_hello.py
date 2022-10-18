2b62ad75ccfffdce4f6d32f31e4cd5316364f67b
faubsam <faubertsamuel@gmail.com>
2022-10-17 20:12:41 -0400

    add test python scripts

---
+++ test_hello.py
@@ -0,0 +1,15 @@
+from hello import toyou, add, subtract
+
+def setup_function(function):
+    print("Running setup: %s" % function.__name__)
+    function.x = 10
+    
+def teardown_function(function):
+    print("Tearing down: %s" % function.__name__)
+    del function.x
+    
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
+    
+def test_hello_add():
+    assert add(test_hello_add.x) == 12
